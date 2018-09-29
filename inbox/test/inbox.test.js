eb045986de8c37003d1091d17be6b8dad63dd3f0
Alex <18474012+agao2@users.noreply.github.com>
2018-09-29 10:39:09 -0700

    async and await instead of promises, deploy contract'

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -4,17 +4,25 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 // web3 is how javascript communicates to an ethereum network
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
+let accounts;
+let inbox;
 
-beforeEach(() => {
+beforeEach(async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts)
-        });
-})
+    accounts = await web3.eth.getAccounts();
 
-describe('Inbox' , () => {
-    it('deploys a contract' , () => {});
+    // use an account to deploy contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello world!'] })
+        .send({ from: accounts[0], gas: '1000000' });
+
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+     });
 });
 
